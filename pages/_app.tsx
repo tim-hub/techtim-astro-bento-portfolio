c62e25c3af52d936192da6bc77f3c2244e69eaea
tim-hub <tim-hub@users.noreply.github.com>
2022-07-16 22:40:02 +1200

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider, Container } from '@chakra-ui/react'
+import { theme } from '../theme'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Container>
+        <Component {...pageProps} />
+      </Container>
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
