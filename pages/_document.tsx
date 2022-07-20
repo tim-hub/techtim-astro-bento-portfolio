a2b955a81c2199e052ab1ba92ff76647535ea0ed
tim-hub <tim-hub@users.noreply.github.com>
2022-07-20 22:08:00 +1200

    do some on head and seo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,10 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html lang='en'>
-        <Head />
+        <Head>
+          <title>{'Tech Tim (@techTim42) - Learn, Share and Grow'}</title>
+          <meta name="description" content="Learn, Share and Grow ❤️ -- Software Engineer | AWS Professional | Open Source Lover  --☘️ Be more productive, Better in communicating, Contribute more to community " />
+        </Head>
         <body>
         {/* 👇 Here's the script */}
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
