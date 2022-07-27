b61ebabd20b2ab89feeb0e5c4a40375f6c298190
tim-hub <tim-hub@users.noreply.github.com>
2022-07-28 11:58:10 +1200

    use redirect to forward to umami

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { ColorModeScript } from '@chakra-ui/react'
 import NextDocument, { Head, Html, Main, NextScript } from 'next/document'
 import Script from 'next/script'
 import { theme } from '../theme'
-import { SITE_ID } from '../utils/configs';
+import { SITE_ID, SITE_URL } from '../utils/configs';
 
 export default class Document extends NextDocument {
   render() {
@@ -23,7 +23,7 @@ export default class Document extends NextDocument {
             async
             defer
             data-website-id="653a53a5-23d1-49e2-a8cf-669aad362b03"
-            src="/umami.js"
+            src={`${SITE_URL}/umami.js`}
           />
         </body>
       </Html>
