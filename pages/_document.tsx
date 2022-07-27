36f37078de65e41e7e7d6605bb7b23c4039b0da5
tim-hub <tim-hub@users.noreply.github.com>
2022-07-28 11:50:36 +1200

    add umami js properly

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,22 +1,30 @@
 import { ColorModeScript } from '@chakra-ui/react'
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
+import NextDocument, { Head, Html, Main, NextScript } from 'next/document'
 import Script from 'next/script'
-import {theme} from '../theme'
+import { theme } from '../theme'
+import { SITE_ID } from '../utils/configs';
 
 export default class Document extends NextDocument {
   render() {
     return (
       <Html lang='en'>
         <Head>
-          <title>{'Tech Tim (@techTim42) - Learn, Share and Grow'}</title>
-          <meta name="description" content="Learn, Share and Grow ❤️ -- Software Engineer | AWS Professional | Open Source Lover  --☘️ Be more productive, Better in communicating, Contribute more to community " />
+          <meta property="og:site_name" content={SITE_ID} />
+          <meta name="description"
+                content="Learn, Share and Grow ❤️ -- Software Engineer | AWS Professional | Open Source Lover  --☘️ Be more productive, Better in communicating, Contribute more to community "/>
         </Head>
         <body>
-        {/* 👇 Here's the script */}
-        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-        <Main />
-        <NextScript />
-        <Script async defer data-website-id="653a53a5-23d1-49e2-a8cf-669aad362b03" src="/umami.js"/>
+          {/* 👇 Here's the script */}
+          <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
+          <Main/>
+          <NextScript/>
+          <Script
+            strategy={'afterInteractive'}
+            async
+            defer
+            data-website-id="653a53a5-23d1-49e2-a8cf-669aad362b03"
+            src="/umami.js"
+          />
         </body>
       </Html>
     )
