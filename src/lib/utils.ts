5ae8cf5d0b123d0655f200a04c3a14104bbf422a
tim-hub <tim-hub@users.noreply.github.com>
2024-09-05 20:00:00 +1200

    update study card

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from 'tailwind-merge'
 export function mergeClassNamesSafely(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const shuffleArray = (array: Array<any>) => {
+  for (let i = array.length - 1; i >= 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
