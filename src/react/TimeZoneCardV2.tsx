d6dcafedcadaad1353be7210cbc0de4859fee615
tim-hub <tim-hub@users.noreply.github.com>
2024-09-05 20:00:00 +1200

    update layout card wrapper and timezone display

--- src/react/TimeZoneCardV2.tsx
+++ src/react/TimeZoneCardV2.tsx
@@ -1,24 +1,27 @@
 import React, { useState, useEffect } from 'react'
 import moment from 'moment-timezone'
 
-const Timezone = ({ timezone }: any) => {
-  const [dateTime, setDateTime] = useState('')
+const Timezone = ({timezone}: any) => {
+  const [dateTime, setDateTime] = useState<Date>(new Date())
+  const updateDuration = 1001; // be naughty
 
   useEffect(() => {
     const interval = setInterval(() => {
       const now = moment().tz(timezone)
-      setDateTime(now.format('dddd, DD MMMM YYYY [a] h:mm:ss A'))
-    }, 1000)
-
+      setDateTime(now.toDate())
+    }, updateDuration)
     return () => clearInterval(interval)
   }, [timezone])
 
-  return (
-    <div>
-      <p>{dateTime}</p>
-      <p className="text-sm text-gray-500">🇨🇴 Bogotá D.C, Colombia 🇨🇴</p>
-    </div>
-  )
+  if (dateTime) {
+    return (
+      <div className={"p-1"}>
+        <p>{dateTime.toDateString()}</p>
+        <p className={'text-2xl my-2'}>{dateTime.toLocaleTimeString()}</p>
+        <p className="text-sm text-gray-500">{timezone}</p>
+      </div>
+    )
+  }
 }
 
 export default Timezone
