c7a5dfc41ccc62c4ecd418976a3a5b903e02c60e
tim-hub <tim-hub@users.noreply.github.com>
2024-09-06 20:00:00 +1200

    migrate from mdx pages to use content

---
+++ src/content/config.ts
@@ -0,0 +1,20 @@
+// Import utilities from `astro:content`
+import { z, defineCollection } from "astro:content";
+// Define a `type` and `schema` for each collection
+const projectCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    startDate: z.date(),
+    description: z.string(),
+    image: z.object({
+      url: z.string(),
+      alt: z.string()
+    }).optional(),
+    tags: z.array(z.string()).optional()
+  })
+});
+// Export a single `collections` object to register your collection(s)
+export const collections = {
+  projects: projectCollection,
+};
