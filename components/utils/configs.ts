7ca53b0d748361aa768ec956a8fd2bb287a62332
tim-hub <tim-hub@users.noreply.github.com>
2024-09-12 20:41:09 +1200

    clean up old nextjs project files

--- components/utils/configs.ts
+++
@@ -1,5 +0,0 @@
-const packageConfig = require('../package.json')
-
-export const SITE_URL = packageConfig.site
-export const SITE_ID = packageConfig.id
-export const SITE_NAME = packageConfig.fullName
