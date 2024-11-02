102518a7afda164f88fcfb627a60334891bdae80
tim-hub <tim-hub@users.noreply.github.com>
2024-11-02 22:15:05 +1300

    update animation
    (cherry picked from commit cf6dc20ea2d70f3b3abfda0d369d55ed82f54ffa)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -66,10 +66,19 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        "appear": {
+           "0%": {
+             opacity: "0",
+           },
+           "100%": {
+             opacity: "1"
+           }
+           }
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "appear": "appear 0.5s ease-in-out"
       },
     },
   },
