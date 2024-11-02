4638154554c3cf7368ba3534291bb3b2ddb51497
tim-hub <tim-hub@users.noreply.github.com>
2024-11-02 22:15:16 +1300

    update now time laoding
    (cherry picked from commit dcefd61a9f77004850d1d285d04c2d85d7e3cf10)

--- src/components/react/NowTime.tsx
+++ src/components/react/NowTime.tsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import moment from "moment-timezone";
-import { getUserTimeZoneInBrowser } from "@/lib/utils.ts";
+import {getUserTimeZoneInBrowser} from "@/lib/utils.ts";
 
 const NowTime = ({timezone}: { timezone?: string }) => {
   const [currentTime, setCurrentTime] = useState<string>('');
@@ -31,27 +31,39 @@ const NowTime = ({timezone}: { timezone?: string }) => {
   const theTime = dateStrings.slice(4).join(' ');
 
 
-  if (currentTime) {
-    return (
-      <>
-        <div>
-          <p>{theDate}</p>
-          <p className={"text-2xl my-2"}>{theTime}</p>
-          <p className="text-sm text-gray-500">{usingTimezone}</p>
-        </div>
-        {!timezone && (
-          <span className="relative flex h-3 w-3">
+  return (
+    <>
+      {
+        currentTime ? <>
+          <div>
+            <p>{theDate}</p>
+            <p className={"text-2xl my-2"}>{theTime}</p>
+            <p className="text-sm text-gray-500">{usingTimezone}</p>
+          </div>
+          {!timezone && (
+            <span className="relative flex h-3 w-3">
             <span
               className="absolute inline-flex h-full w-full animate-ping rounded-full bg-green-400 opacity-75"
             ></span>
             <span className="relative inline-flex h-3 w-3 rounded-full bg-green-500"></span>
           </span>
-        )}
-      </>
+          )}
+        </> : <div className="h-[100px] w-full">
+          <div className="flex-1 space-y-6 py-1">
+            <div className="h-2 bg-slate-200 rounded"></div>
+            <div className="space-y-3">
+              <div className="grid grid-cols-3 gap-4">
+                <div className="h-2 bg-slate-200 rounded col-span-2"></div>
+                <div className="h-2 bg-slate-200 rounded col-span-1"></div>
+              </div>
+              <div className="h-2 bg-slate-200 rounded"></div>
+            </div>
+          </div>
+        </div>
+      }
+    </>
+  );
 
-    )
-      ;
-  }
 };
 
 export default NowTime;
